778c74318371a8a5ef0b4bb0c6ae8f762faca164
Abu David <davidabu80@gmail.com>
2022-04-26 23:25:02 +0100

    Updates

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,8 +1,18 @@
 pragma solidity ^0.8.0;
 contract SimpleStorage {
     uint256 public myNum;
+    struct People{
+        uint256 num;
+        string name;
+    }
+    People public person  = People({
+        num: 4,
+        name: "Abu Dhabi"
+    })
+    People[] public person;
     function store(uint256 _num) public {
         myNum = _num;
+        person.push(myNum);
     }
     function retrieve() public view returns(uint256){
         return myNum;
